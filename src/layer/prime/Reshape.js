eb48527a37297cb9ca835487d69d591ae29799c8
syt123450 <421537891@qq.com>
2018-09-06 22:00:43 -0700

    add new reshape layer

--- src/layer/prime/Reshape.js
+++ src/layer/prime/Reshape.js
@@ -1,82 +1,350 @@
 import {Layer } from "./Layer";
+import {fmCenterGenerator} from "../../utils/FmCenterGenerator";
+import { MapTransitionFactory } from "../../animation/MapTransitionTween";
+import {FeatureMap} from "../../elements/FeatureMap";
+import {MapAggregation} from "../../elements/MapAggregation";
+import {colorUtils} from "../../utils/ColorUtils";
+import {MapDataGenerator} from "../../utils/MapDataGenerator";
 
 function Reshape(config) {
 
 	Layer.call(this, config);
 
+	this.targetShape = undefined;
+	this.width = undefined;
+	this.height = undefined;
+	this.depth = undefined;
+
+	// set init size to be 1
+	this.totalSize = 1;
+	this.inputShape = undefined;
+
+	this.fmCenters = [];
+	this.openFmCenters = [];
+	this.closeFmCenters = [];
+	this.aggregationStrategy = undefined;
+
+	this.loadLayerConfig(config);
+
+	this.layerType = "reshape";
+
 }
 
 Reshape.prototype = Object.assign(Object.create(Layer.prototype), {
 
-	init: function() {
+	init: function(center, actualDepth, nextHookHandler) {
+
+		this.center = center;
+		this.actualDepth = actualDepth;
+		this.nextHookHandler = nextHookHandler;
+		this.lastHookHandler = this.lastLayer.nextHookHandler;
+
+		this.neuralGroup = new THREE.Group();
+		this.neuralGroup.position.set(this.center.x, this.center.y, this.center.z);
+
+		if (this.isOpen) {
+
+			for (let i = 0; i < this.openFmCenters.length; i++) {
+				this.fmCenters.push(this.openFmCenters[i]);
+			}
+			this.initSegregationElements(this.openFmCenters);
+			this.initCloseButton();
+
+		} else {
+
+			this.initAggregationElement();
+
+		}
+
+		this.scene.add(this.neuralGroup);
 
 	},
 
-	loadModelConfig: function() {
+	loadLayerConfig: function(layerConfig) {
+
+		if (layerConfig !== undefined) {
+
+			if (layerConfig.targetShape !== undefined) {
+				this.targetShape = layerConfig.targetShape;
+				this.width = layerConfig.targetShape[0];
+				this.height = layerConfig.targetShape[1];
+			} else {
+				console.error("\"targetShape\" property is required for reshape layer");
+			}
+
+		}
 
 	},
 
-	assemble: function() {
+	loadModelConfig: function(modelConfig) {
+
+		if (this.isOpen === undefined) {
+			this.isOpen = modelConfig.layerInitStatus;
+		}
+
+		if (this.color === undefined) {
+			this.color = modelConfig.color.pooling2d;
+		}
+
+		if (this.layerShape === undefined) {
+			this.layerShape = modelConfig.layerShape;
+		}
+
+		if (this.relationSystem === undefined) {
+			this.relationSystem = modelConfig.relationSystem;
+		}
+
+		if (this.textSystem === undefined) {
+			this.textSystem = modelConfig.textSystem;
+		}
+
+		if (this.aggregationStrategy === undefined) {
+			this.aggregationStrategy = modelConfig.aggregationStrategy;
+		}
+
+	},
+
+	assemble: function(layerIndex) {
+
+		this.layerIndex = layerIndex;
+
+		this.inputShape = this.lastLayer.outputShape;
+
+		for (let i = 0; i < this.inputShape.length; i++) {
+			this.totalSize *= this.inputShape[i];
+		}
+
+		this.depth = this.totalSize / (this.width * this.height);
+
+		for (let i = 0; i < this.depth; i++) {
+			let closeFmCenter = {
+				x: 0,
+				y: 0,
+				z: 0
+			};
+			this.closeFmCenters.push(closeFmCenter);
+		}
+
+		this.realVirtualRatio = this.lastLayer.realVirtualRatio;
+		this.actualWidth = this.width * this.realVirtualRatio;
+		this.actualHeight = this.height * this.realVirtualRatio;
+
+		this.openFmCenters = fmCenterGenerator.getFmCenters(this.layerShape, this.depth, this.actualWidth, this.actualHeight);
+
+		this.leftMostCenter = this.openFmCenters[0];
+		this.openHeight = this.actualHeight + this.openFmCenters[this.openFmCenters.length - 1].z - this.openFmCenters[0].z;
 
 	},
 
 	openLayer: function() {
 
+		console.log("open layer");
+
+		if (!this.isOpen) {
+
+			this.disposeAggregationElement();
+			this.initSegregationElements(this.closeFmCenters);
+			MapTransitionFactory.openLayer(this);
+		}
+
 	},
 
 	closeLayer: function() {
+
+		console.log("close layer");
+
+		if (this.isOpen) {
+
+			MapTransitionFactory.closeLayer(this);
+
+		}
 
 	},
 
 	initAggregationElement: function() {
 
+		let aggregationHandler = new MapAggregation(
+			this.width,
+			this.height,
+			this.actualWidth,
+			this.actualHeight,
+			this.actualDepth,
+			this.color
+		);
+		aggregationHandler.setLayerIndex(this.layerIndex);
+
+		this.aggregationHandler = aggregationHandler;
+		this.neuralGroup.add(aggregationHandler.getElement());
+
+		if (this.neuralValue !== undefined) {
+			this.updateAggregationVis();
+		}
 	},
 
 	disposeAggregationElement: function() {
 
+		this.neuralGroup.remove(this.aggregationHandler.getElement());
+		this.aggregationHandler = undefined;
+
 	},
 
-	initSegregationElements: function() {
+	initSegregationElements: function(centers) {
+
+		for (let i = 0; i < this.depth; i++) {
+
+			let segregationHandler = new FeatureMap(
+				this.width,
+				this.height,
+				this.actualWidth,
+				this.actualHeight,
+				centers[i],
+				this.color
+			);
+
+			segregationHandler.setLayerIndex(this.layerIndex);
+			segregationHandler.setFmIndex(i);
+
+			this.segregationHandlers.push(segregationHandler);
+
+			this.neuralGroup.add(segregationHandler.getElement());
+
+		}
+
+		if (this.neuralValue !== undefined) {
+			this.updateSegregationVis();
+		}
 
 	},
 
 	disposeSegregationElements: function() {
 
+		for (let i = 0; i < this.segregationHandlers.length; i++) {
+			this.neuralGroup.remove(this.segregationHandlers[i].getElement());
+		}
+
+		this.segregationHandlers = [];
+
 	},
 
-	updateValue: function() {
+	updateValue: function(value) {
+
+		this.neuralValue = value;
+
+		if (this.isOpen) {
+			this.updateSegregationVis();
+		} else {
+			this.updateAggregationVis();
+		}
 
 	},
 
 	updateAggregationVis: function() {
 
+		let aggregationUpdateValue = MapDataGenerator.generateAggregationData(this.neuralValue, this.depth, this.aggregationStrategy);
+
+		let colors = colorUtils.getAdjustValues(aggregationUpdateValue);
+
+		this.aggregationHandler.updateVis(colors);
+
 	},
 
 	updateSegregationVis: function() {
 
+		let layerOutputValues = MapDataGenerator.generateChannelData(this.neuralValue, this.depth);
+
+		let colors = colorUtils.getAdjustValues(layerOutputValues);
+
+		let featureMapSize = this.width * this.height;
+
+		for (let i = 0; i < this.depth; i++) {
+
+			this.segregationHandlers[i].updateVis(colors.slice(i * featureMapSize, (i + 1) * featureMapSize));
+
+		}
+
 	},
 
-	handleClick: function() {
+	handleClick: function(clickedElement) {
+
+		if (clickedElement.elementType === "aggregationElement") {
+			this.openLayer();
+		} else if (clickedElement.elementType === "closeButton") {
+			this.closeLayer();
+		}
 
 	},
 
-	handleHoverIn: function() {
+	handleHoverIn: function(hoveredElement) {
+
+		if (this.relationSystem !== undefined && this.relationSystem) {
+			this.initLineGroup(hoveredElement);
+		}
+
+		if (this.textSystem !== undefined && this.textSystem) {
+			this.showText(hoveredElement);
+		}
 
 	},
 
 	handleHoverOut: function() {
 
+		this.disposeLineGroup();
+		this.hideText();
+
 	},
 
-	getRelativeElements: function() {
+	getRelativeElements: function(selectedElement) {
+
+		let relativeElements = [];
+
+		if (selectedElement.elementType === "aggregationElement") {
+
+			if (this.lastLayer.isOpen) {
+
+				for (let i = 0; i < this.lastLayer.segregationHandlers.length; i++) {
+					relativeElements.push(this.lastLayer.segregationHandlers[i].getElement());
+				}
+
+			} else {
+
+				relativeElements.push(this.lastLayer.aggregationHandler.getElement());
+
+			}
+
+		} else if (selectedElement.elementType === "featureMap") {
+
+			// as reshape layer's feature map number is different with last layer, so if last layer is opened, will not show relation lines
+
+			if (!this.lastLayer.isOpen) {
+
+				relativeElements.push(this.lastLayer.aggregationHandler.getElement());
+
+			}
+
+		}
+
+		return relativeElements;
+
 
 	},
 
-	showText: function() {
+	showText: function(element) {
+
+		if (element.elementType === "featureMap") {
+
+			let fmIndex = element.fmIndex;
+			this.segregationHandlers[fmIndex].showText();
+			this.textElementHandler = this.segregationHandlers[fmIndex];
+		}
 
 	},
 
 	hideText: function() {
+
+		if (this.textElementHandler !== undefined) {
+			this.textElementHandler.hideText();
+			this.textElementHandler = undefined;
+		}
 
 	}
 
