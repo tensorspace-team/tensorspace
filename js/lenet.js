e4dde7e5d05f1aa35e5d6517907a6fed1539ee8c
syt123450 <421537891@qq.com>
2018-10-19 00:55:15 -0700

    yolo example

--- js/lenet.js
+++ js/lenet.js
@@ -1,4 +1,5 @@
-let model
+let model;
+let signaturePad;
 
 $(function() {
 
@@ -27,6 +28,15 @@ $(function() {
 		clearResult();
 
 	});
+
+	signaturePad = new SignaturePad( document.getElementById( 'signature-pad' ), {
+
+		minWidth: 10,
+		backgroundColor: 'rgba(255, 255, 255, 0)',
+		penColor: 'rgb(214, 253, 255)',
+		onEnd: executePredict
+
+	} );
 
 });
 
@@ -168,5 +178,69 @@ function moveOutHiddenContent() {
 		left:"-=200px"
 	},500);
 	$("#curtain").fadeOut(500);
+
+}
+
+function executePredict() {
+
+	let canvas = document.getElementById( "signature-pad" );
+	let context = canvas.getContext( '2d' );
+	let imgData = context.getImageData( 0, 0, canvas.width, canvas.height );
+
+	let signatureData = [];
+
+	for ( let i = 0; i < 224; i += 8 ) {
+
+		for ( let j = 3; j < 896; j += 32 ) {
+
+			signatureData.push( imgData.data[ 896 * i + j ] / 255 );
+
+		}
+
+	}
+
+	model.predict( signatureData, function( predictResult ) {
+
+		let index = getMaxConfidentNumber(predictResult);
+		clearResult();
+		highLightResult(index);
+
+	});
+
+}
+
+function getMaxConfidentNumber( predictResult ) {
+
+	let maxIndex = 0;
+
+	for ( let i = 1; i < predictResult.length; i ++ ) {
+
+		maxIndex = predictResult[ i ] > predictResult[ maxIndex ] ? i : maxIndex;
+
+	}
+
+	return maxIndex;
+
+}
+
+function highLightResult( index ) {
+
+	let idString = "result" + index;
+	$("#" + idString).css( {
+		"color": "#D6FDFF"
+	} );
+
+}
+
+function clearResult() {
+
+	for ( let i = 0; i < 10; i ++ ) {
+
+		let idString = "result" + i;
+		$("#" + idString).css({
+			"color": "#456989"
+		});
+
+	}
 
 }
