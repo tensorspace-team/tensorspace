ecf48ed6dd100dd5f700a6d8a95c9c28ae72bbac
syt123450 <421537891@qq.com>
2018-11-30 11:20:31 -0800

    paging feature for input1d (#143)

--- src/layer/input/Input1d.js
+++ src/layer/input/Input1d.js
@@ -6,6 +6,8 @@ import { NativeLayer } from "../abstract/NativeLayer";
 import { ModelInitWidth } from "../../utils/Constant";
 import { ColorUtils } from "../../utils/ColorUtils";
 import { NeuralQueue } from "../../elements/NeuralQueue";
+import { QueueSegment } from "../../elements/QueueSegment";
+import { PaginationButton } from "../../elements/PagniationButton";
 
 /**
  * Input1d, input layer, can be initialized by TensorSpace user.
@@ -38,6 +40,71 @@ function Input1d( config ) {
 	 */
 
 	this.depth = 1;
+
+	/**
+	 * Decide how to display hint text.
+	 *
+	 * @type { boolean }
+	 */
+
+	this.overview = false;
+
+	/**
+	 * mode for how to display queue element
+	 * If queue element is too long, use "paging" mode may have better visualization effect.
+	 *
+	 * @type { boolean }
+	 */
+
+	this.paging = false;
+
+	/**
+	 * Only take effect when this.paging = true.
+	 * Segment length for "one page".
+	 * Default to 200.
+	 *
+	 * @type { number }
+	 */
+
+	this.segmentLength = 200;
+
+	/**
+	 * Only take effect when this.paging = true.
+	 * Which page NativeLayer1d displays now.
+	 * Can be update when "last" or "next" buttons are clicked, initial value can be defined by user.
+	 * Default to 0.
+	 *
+	 * @type { number }
+	 */
+
+	this.segmentIndex = 0;
+
+	/**
+	 * Only take effect when this.paging = true.
+	 * How many pages in NativeLayer1d.
+	 *
+	 * @type { number }
+	 */
+
+	this.totalSegments = undefined;
+
+	/**
+	 * Only take effect when this.paging = true.
+	 * Store handler for last button.
+	 *
+	 * @type { Object }
+	 */
+
+	this.lastButtonHandler = undefined;
+
+	/**
+	 * Only take effect when this.paging = true.
+	 * Store handler for next button.
+	 *
+	 * @type { Object }
+	 */
+
+	this.nextButtonHandler = undefined;
 
 	// Load user's Input1d configuration.
 
@@ -125,6 +192,14 @@ Input1d.prototype = Object.assign( Object.create( NativeLayer.prototype ), {
 
 		this.initAggregationElement();
 
+		if ( this.paging ) {
+
+			// Init pagination button when layer is in "paging mode".
+
+			this.showPaginationButton();
+
+		}
+
 		// Add the wrapper object to the actual THREE.js scene.
 
 		this.scene.add( this.neuralGroup );
@@ -163,6 +238,25 @@ Input1d.prototype = Object.assign( Object.create( NativeLayer.prototype ), {
 		// aggregationHandler execute update visualization process.
 
 		this.aggregationHandler.updateVis( colors );
+
+		if ( this.paging ) {
+
+			// Get part of colors to render segment.
+
+			let segmentColors = colors.slice(
+
+				this.segmentLength * this.segmentIndex,
+				Math.min( this.segmentLength * ( this.segmentIndex + 1 ), this.width - 1 )
+
+			);
+
+			this.aggregationHandler.updateVis( segmentColors );
+
+		} else {
+
+			this.aggregationHandler.updateVis( colors );
+
+		}
 
 	},
 
@@ -315,6 +409,37 @@ Input1d.prototype = Object.assign( Object.create( NativeLayer.prototype ), {
 
 			}
 
+			if ( layerConfig.paging !== undefined ) {
+
+				this.paging = layerConfig.paging;
+
+				// If paging mode is set, load paging parameters.
+
+				if ( this.paging ) {
+
+					if ( layerConfig.segmentLength !== undefined ) {
+
+						this.segmentLength = layerConfig.segmentLength;
+						this.queueLength = this.segmentLength;
+
+					}
+
+					if ( layerConfig.initSegmentIndex !== undefined ) {
+
+						this.segmentIndex = layerConfig.initSegmentIndex;
+
+					}
+
+				}
+
+			}
+
+			if ( layerConfig.overview !== undefined ) {
+
+				this.overview = layerConfig.overview;
+
+			}
+
 		} else {
 
 			console.error( "Lack config for Input1d layer." );
@@ -331,14 +456,37 @@ Input1d.prototype = Object.assign( Object.create( NativeLayer.prototype ), {
 
 		// NeuralQueue Object is a wrapper for linear input element, checkout "NeuralQueue.js" for more information.
 
-		let aggregationHandler = new NeuralQueue(
+		let aggregationHandler;
 
-			this.width,
-			this.unitLength,
-			this.color,
-			this.minOpacity
+		if ( this.paging ) {
 
-		);
+			aggregationHandler = new QueueSegment(
+
+				this.segmentLength,
+				this.segmentIndex,
+				this.width,
+				this.unitLength,
+				this.color,
+				this.minOpacity,
+				this.overview
+
+			);
+
+			this.queueLength = aggregationHandler.queueLength;
+
+		} else {
+
+			aggregationHandler = new NeuralQueue(
+
+				this.width,
+				this.unitLength,
+				this.color,
+				this.minOpacity,
+				this.overview
+
+			);
+
+		}
 
 		// Set layer index to aggregation, linear input element can know which layer it has been positioned.
 
@@ -351,6 +499,24 @@ Input1d.prototype = Object.assign( Object.create( NativeLayer.prototype ), {
 		// Get actual THREE.js element and add it to layer wrapper Object.
 
 		this.neuralGroup.add( aggregationHandler.getElement() );
+
+	},
+
+	/**
+	 * handleClick() If clickable element in this layer is clicked, execute this handle function.
+	 *
+	 * @param { THREE.Object } clickedElement, clicked element picked by model's Raycaster.
+	 */
+
+	handleClick: function( clickedElement ) {
+
+		if ( clickedElement.elementType === "paginationButton" ) {
+
+			// If pagination button ("last" or "next") is clicked, update page visualization.
+
+			this.updatePage( clickedElement.paginationType );
+
+		}
 
 	},
 
@@ -381,6 +547,282 @@ Input1d.prototype = Object.assign( Object.create( NativeLayer.prototype ), {
 
 			this.textElementHandler.hideText();
 			this.textElementHandler = undefined;
+
+		}
+
+	},
+
+	/**
+	 * calcPaginationButtonSize() calculate button size.
+	 *
+	 * @return { number } size, pagination button size
+	 */
+
+	calcPaginationButtonPos: function( paginationType ) {
+
+		if ( paginationType === "last" ) {
+
+			// "last" button is positioned in the left of the layer.
+
+			return {
+
+				x: - this.queueLength * this.unitLength / 2 - 5 * this.unitLength,
+				y: 0,
+				z: 0
+
+			};
+
+		} else {
+
+			// "next" button is positioned in the right of the layer.
+
+			return {
+
+				x: this.queueLength * this.unitLength / 2 + 5 * this.unitLength,
+				y: 0,
+				z: 0
+
+			};
+
+		}
+
+	},
+
+	/**
+	 * calcPaginationButtonSize() calculate button size.
+	 *
+	 * @return { number } size, pagination button size
+	 */
+
+	calcPaginationButtonSize: function() {
+
+		// To make close button's size responsive, width = 50 is the boundary.
+
+		if ( this.width > 50 ) {
+
+			return 2 * this.unitLength;
+
+		} else {
+
+			return 1.1 * this.unitLength;
+
+		}
+
+	},
+
+	/**
+	 * showPaginationButton() conditional add "next" button and "last" button into layer1d.
+	 */
+
+	showPaginationButton: function() {
+
+		if ( this.segmentIndex === 0 && this.segmentIndex !== this.totalSegments - 1 ) {
+
+			// First page only show "next" button.
+
+			this.showNextButton();
+
+		} else if ( this.segmentIndex !== 0 && this.segmentIndex === this.totalSegments - 1 ) {
+
+			// last page only show "last" button.
+
+			this.showLastButton();
+
+		} else if ( this.segmentIndex === 0 && this.segmentIndex === this.totalSegments - 1 ) {
+
+			// If only has one page, no button.
+
+		} else {
+
+			// In other situational, show two button.
+
+			this.showNextButton();
+			this.showLastButton();
+
+		}
+
+	},
+
+	/**
+	 * showLastButton() initialize "last" button, and add it to neuralGroup.
+	 */
+
+	showLastButton: function() {
+
+		let lastButtonHandler = new PaginationButton(
+
+			"last",
+			this.calcPaginationButtonSize(),
+			this.unitLength,
+			this.calcPaginationButtonPos( "last" ),
+			this.color,
+			this.minOpacity
+
+		);
+
+		// Set layer index to "last" button, button object can know which layer it has been positioned.
+
+		lastButtonHandler.setLayerIndex( this.layerIndex );
+
+		this.lastButtonHandler = lastButtonHandler;
+		this.neuralGroup.add( this.lastButtonHandler.getElement() );
+
+	},
+
+	/**
+	 * showNextButton() initialize "next" button, and add it to neuralGroup.
+	 */
+
+	showNextButton: function() {
+
+		let nextButtonHandler = new PaginationButton(
+
+			"next",
+			this.calcPaginationButtonSize(),
+			this.unitLength,
+			this.calcPaginationButtonPos( "next" ),
+			this.color,
+			this.minOpacity
+
+		);
+
+		// Set layer index to "next" button, button object can know which layer it has been positioned.
+
+		nextButtonHandler.setLayerIndex( this.layerIndex );
+
+		this.nextButtonHandler = nextButtonHandler;
+		this.neuralGroup.add( this.nextButtonHandler.getElement() );
+
+	},
+
+	/**
+	 * hidePaginationButton(), hide "last" button and "next" button.
+	 */
+
+	hidePaginationButton: function() {
+
+		this.hideNextButton();
+		this.hideLastButton();
+
+	},
+
+	/**
+	 * hideNextButton(), hide "next" button.
+	 */
+
+	hideNextButton: function() {
+
+		if ( this.nextButtonHandler !== undefined ) {
+
+			this.neuralGroup.remove( this.nextButtonHandler.getElement() );
+			this.nextButtonHandler = undefined;
+
+		}
+
+	},
+
+	/**
+	 * hideLastButton(), hide "last" button.
+	 */
+
+	hideLastButton: function() {
+
+		if ( this.lastButtonHandler !== undefined ) {
+
+			this.neuralGroup.remove( this.lastButtonHandler.getElement() );
+			this.lastButtonHandler = undefined;
+
+		}
+
+	},
+
+	/**                                                                                                                                                 y        y                        /**
+	 * updatePage() execute actual page update work.
+	 *
+	 * @param { string } paginationType, "last" or "next".
+	 */
+
+	updatePage: function( paginationType ) {
+
+		if ( paginationType === "next" ) {
+
+			// "next" button is clicked.
+
+			if ( this.segmentIndex === 0 ) {
+
+				// First page now, click "next" button will show "last" button.
+
+				this.showLastButton();
+
+			}
+
+			if ( this.segmentIndex === this.totalSegments - 2 ) {
+
+				// Is going to the last page, the last page do not have "next" button.
+
+				this.hideNextButton();
+
+			}
+
+			// Update segmentIndex.
+
+			this.segmentIndex += 1;
+
+		} else {
+
+			// "last" button is clicked.
+
+			if ( this.segmentIndex === this.totalSegments - 1 ) {
+
+				// The Last page now, click "last" button will show "next" button.
+
+				this.showNextButton();
+
+			}
+
+			if ( this.segmentIndex === 1 ) {
+
+				// Is going to the first page, the first page do not have "last" button.
+
+				this.hideLastButton();
+
+			}
+
+			// Update segmentIndex.
+
+			this.segmentIndex -= 1;
+
+		}
+
+		// Modify segment element based on new segment index.
+
+		this.aggregationHandler.updateSegmentIndex( this.segmentIndex );
+
+		// Check whether queue length change, situation: the page's length may different with previous page.
+
+		if ( this.aggregationHandler.isLengthChanged ) {
+
+			this.queueLength = this.aggregationHandler.queueLength;
+
+			if ( this.nextButtonHandler !== undefined ) {
+
+				let nextButtonPos = this.calcPaginationButtonPos( "next" );
+				this.nextButtonHandler.updatePos( nextButtonPos );
+
+			}
+
+			if ( this.lastButtonHandler !== undefined ) {
+
+				let lastButtonPos = this.calcPaginationButtonPos( "last" );
+				this.lastButtonHandler.updatePos( lastButtonPos );
+
+			}
+
+		}
+
+		if ( this.neuralValue !== undefined ) {
+
+			this.updateQueueVis();
 
 		}
 
