1d1591dad9d151be0ec477e5b5b25ca6119b35a9
botime <lb8166@gmail.com>
2019-01-08 09:24:24 -0800

    Create rollup.config.prod.js
    Switch to terser.js from uglify-es to minify source code for
    distribution.
    
    Reason:
    "uglify-es is no longer maintained and uglify-js does not support ES6+."
    refer to: https://github.com/terser-js/terser#why-choose-terser

---
+++ rollup.config.prod.js
@@ -0,0 +1,27 @@
+/**
+ * Created by ss on 2018/01/06.
+ */
+import { terser } from "rollup-plugin-terser";
+
+let terserOptions = {
+    output: {
+        ecma: 5,
+        preamble: '// https://github.com/tensorspace-team/tensorspace/blob/master/LICENSE',
+    },
+}
+
+export default [
+    // Build minified version for distribution
+    {
+        input: 'src/tensorspace.js',
+        plugins: [terser(terserOptions)],
+        output: [
+            {
+                format: 'iife',
+                file: 'build/tensorspace.min.js',
+                name: "TSP",
+                sourcemap: true,
+            }
+        ],
+    },
+]
