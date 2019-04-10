16113c6537c904b2d1ea532a30451b9039d14b0d
syt123450 <421537891@qq.com>
2019-04-10 04:20:34 -0700

    Example: Update angular helloworld with new API

--- examples/helloworld-angular/src/app/app.component.ts
+++ examples/helloworld-angular/src/app/app.component.ts
@@ -17,16 +17,15 @@ export class AppComponent implements AfterViewInit {
     const container = document.getElementById('container');
     const model = new TSP.models.Sequential( container );
 
-    model.add( new TSP.layers.GreyscaleInput({ shape: [28, 28, 1] }) );
-    model.add( new TSP.layers.Padding2d({ padding: [2, 2] }) );
-    model.add( new TSP.layers.Conv2d({ kernelSize: 5, filters: 6, strides: 1 }) );
-    model.add( new TSP.layers.Pooling2d({ poolSize: [2, 2], strides: [2, 2] }) );
-    model.add( new TSP.layers.Conv2d({ kernelSize: 5, filters: 16, strides: 1 }) );
-    model.add( new TSP.layers.Pooling2d({ poolSize: [2, 2], strides: [2, 2] }) );
-    model.add( new TSP.layers.Dense({ units: 120 }) );
-    model.add( new TSP.layers.Dense({ units: 84 }) );
+    model.add( new TSP.layers.GreyscaleInput() );
+    model.add( new TSP.layers.Padding2d() );
+    model.add( new TSP.layers.Conv2d() );
+    model.add( new TSP.layers.Pooling2d() );
+    model.add( new TSP.layers.Conv2d() );
+    model.add( new TSP.layers.Pooling2d() );
+    model.add( new TSP.layers.Dense() );
+    model.add( new TSP.layers.Dense() );
     model.add( new TSP.layers.Output1d({
-      units: 10,
       outputs: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     }) );
 
