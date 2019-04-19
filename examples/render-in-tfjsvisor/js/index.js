d4aa7ea2dc40d88c9cbe93a58a75e95444ae8acb
syt123450 <421537891@qq.com>
2019-04-19 13:03:53 -0700

    Example: Update tfjs-vis example to make it compatible with 0.6

--- examples/render-in-tfjsvisor/js/index.js
+++ examples/render-in-tfjsvisor/js/index.js
@@ -66,39 +66,17 @@ function createTensorSpaceModel( surface ) {
 		stats: true
 	} );
 	
-	tspModel.add( new TSP.layers.GreyscaleInput({
-		shape: [28, 28, 1]
-	}) );
-	tspModel.add( new TSP.layers.Padding2d({
-		padding: [2, 2]
-	}) );
+	tspModel.add( new TSP.layers.GreyscaleInput() );
+	tspModel.add( new TSP.layers.Padding2d() );
 	tspModel.add( new TSP.layers.Conv2d({
-		kernelSize: 5,
-		filters: 6,
-		strides: 1,
 		initStatus: "open"
 	}) );
-	tspModel.add( new TSP.layers.Pooling2d({
-		poolSize: [2, 2],
-		strides: [2, 2]
-	}) );
-	tspModel.add( new TSP.layers.Conv2d({
-		kernelSize: 5,
-		filters: 16,
-		strides: 1
-	}) );
-	tspModel.add( new TSP.layers.Pooling2d({
-		poolSize: [2, 2],
-		strides: [2, 2]
-	}) );
-	tspModel.add( new TSP.layers.Dense({
-		units: 120
-	}) );
-	tspModel.add( new TSP.layers.Dense({
-		units: 84
-	}) );
+	tspModel.add( new TSP.layers.Pooling2d() );
+	tspModel.add( new TSP.layers.Conv2d() );
+	tspModel.add( new TSP.layers.Pooling2d() );
+	tspModel.add( new TSP.layers.Dense() );
+	tspModel.add( new TSP.layers.Dense() );
 	tspModel.add( new TSP.layers.Output1d({
-		units: 10,
 		outputs: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
 		initStatus: "open"
 	}) );
